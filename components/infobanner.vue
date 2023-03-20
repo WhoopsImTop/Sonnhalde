07023c8300192e26864d7c5ae3e8fbae354af9d7
Lea <eliasenglen2@gmail.com>
2023-03-20 09:10:55 +0100

    add new

--- components/infobanner.vue
+++ components/infobanner.vue
@@ -3,8 +3,8 @@
     <button class="button" @click="close">
       <img width="25px" src="close.svg" />
     </button>
-    <h3>1. Bauabschnitt</h3>
-    <h4>50% verkauft!</h4>
+    <h4>1. Bauabschnitt 50% verkauft!</h4>
+    <h5 style="color: #ffffff; margin: 10px 0 0">Förderfähig als “Klimafreundlicher Neubau” (KfW)</h5>
   </div>
 </template>
 
